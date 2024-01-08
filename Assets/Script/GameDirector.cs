06c9f539fba251be1dcedc4c6db3d02697f51eb8
龍希 宮川 <keisen_ryuki@outlook.jp>
2024-01-08 22:01:18 +0900

    ver0.30 壁の生成を自動化 　　　 タイトル画面を追加 　　　クリア画面仮実装

--- Assets/Script/GameDirector.cs
+++ Assets/Script/GameDirector.cs
@@ -2,18 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Build.Content;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameDirector : MonoBehaviour
 {
+    GameObject[] items;
+    [SerializeField] TextMeshProUGUI clearText;
+    [SerializeField] GameObject player;
+    PlayerController pc;
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
+        pc = player.GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        items = GameObject.FindGameObjectsWithTag("HardObject");
         
+        if(items.Length == 0)
+        {
+            pc.state = PlayerController.State.Clear;
+            Invoke("AfterGameClear", 2.0f);
+            clearText.text = "Game Clear!!!";
+        }
+    }
+
+    void AfterGameClear()
+    {
+        SceneManager.LoadScene("TitleScene");
     }
 }
